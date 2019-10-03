e7054c4a35f180bab04bea12fa147ef78179d674
Andrew Thiessen <squaids449@gmail.com>
2019-10-03 14:24:34 -0600

    css improvements

--- games-front/search.js
+++ games-front/search.js
@@ -15,7 +15,6 @@ function getLastUser(users){
 }
 
 
-
 gFilter.addEventListener("change", (filter) => {
     gResult = filter.target.value
 })
@@ -32,29 +31,47 @@ submit.addEventListener("click", event => {
 function createCards(games){
     let h2 = document.createElement("h2")
     let div = document.createElement("div")
+    const div3 = document.createElement("div")
     body.innerHTML = ""
+    div3.innerHTML = `
+        <nav id="searchNav">
+            <ul>
+                <li id="brand"><a href="http://localhost:5500/games-front/index.html">Better Than Steam</a></li>
+                <div class="main-nav">
+                        <li><a href="http://localhost:5500/games-front/index.html"><abbr title="Go back to home">Home</abbr></a></li>
+                        <li><a href="http://localhost:5500/games-front/about.html">About</a></li>
+                        <li><a href="http://localhost:5500/games-front/search.html">Back to Search Page</a></li>
+                </div>
+            </ul>
+        </nav>`
     h2.innerText = "Recommendations"
     div.className = "cards"
     games.filter(game => game.genre != null).forEach(game => {
+        let div2 = document.createElement("div")
         let h3 = document.createElement("h3")
         let p1 = document.createElement("p")
         let p2 = document.createElement("p")
         let p3 = document.createElement("p")
         let p4 = document.createElement("p")
+        let p5 = document.createElement("img")
+
         if (game.genre.includes(gResult) && game.platform.toLowerCase().includes(pResult)) {
             h3.innerText = game.name
+            div2.classList.add("game")
             p1.classList.add("game-info")
             p2.classList.add("game-info")
             p3.classList.add("game-info")
             p4.classList.add("game-info")            
-            p1.innerText = `Rating: ${game.rating}`
+            p1.innerText = `Rating: ${game.rating}%`
             p2.innerText = `Genre: ${game.genre}`
             p3.innerText = `Platform: ${game.platform}`
             p4.innerText = `Summary: ${game.summary}`
         }
-        div.append(h3, p1, p2, p3, p4)
+        div2.append(h3, p1, p2, p3, p4)
+
+        div.appendChild(div2)
     })
-    body.append(h2, div)
+    body.append(div3, h2, div)
 }
 
 function makeModel(models){
